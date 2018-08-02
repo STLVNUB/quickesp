afa7fe1fcea56cbb32db20487624cdcc306fe749
Dennis Koluris <dkoluris@outlook.com>
2018-08-02 11:25:31 +0300

    New icon.

--- QuickESP/Main.h
+++ QuickESP/Main.h
@@ -2,4 +2,7 @@
 
 @property (weak) IBOutlet NSWindow *window;
 
+@property (strong, nonatomic) NSStatusItem *statusBar;
+@property (weak) IBOutlet NSMenu *statusMenu;
+
 @end
--- QuickESP/Main.m
+++ QuickESP/Main.m
@@ -4,6 +4,16 @@
 @implementation Main
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    self.statusBar = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
+    //self.statusBar.title = @"G";
+    
+    NSImage *image = [NSImage imageNamed:@"Options"];
+    image.size = NSMakeSize(17.0, 17.0);
+    image.template = YES;
+    self.statusBar.image = image;
+    self.statusBar.menu = self.statusMenu;
+    self.statusBar.highlightMode = YES;
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
